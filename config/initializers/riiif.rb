93e10049110e5e84b5c7a23017d340b161410c90
Stuart Kenny <kennystu@gmail.com>
2023-05-16 17:42:11 +0100

    Make riiif work in development

--- config/initializers/riiif.rb
+++ config/initializers/riiif.rb
@@ -1,7 +1,9 @@
-Riiif::Image.file_resolver = Spotlight::CarrierwaveFileResolver.new
+ActiveSupport::Reloader.to_prepare do
+  Riiif::Image.file_resolver = Spotlight::CarrierwaveFileResolver.new
 
-# Riiif::Image.authorization_service = IIIFAuthorizationService
+  # Riiif::Image.authorization_service = IIIFAuthorizationService
 
-# Riiif.not_found_image = 'app/assets/images/us_404.svg'
-#
-Riiif::Engine.config.cache_duration_in_days = 365
+  # Riiif.not_found_image = 'app/assets/images/us_404.svg'
+  #
+  Riiif::Engine.config.cache_duration_in_days = 365
+end
