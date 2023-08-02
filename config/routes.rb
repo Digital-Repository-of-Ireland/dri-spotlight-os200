c26e49091273bdc3f295781b545caa010071b1c4
Stuart Kenny <kennystu@gmail.com>
2023-08-02 19:50:03 +0100

    Remove maps controller

--- config/routes.rb
+++ config/routes.rb
@@ -28,6 +28,5 @@ Rails.application.routes.draw do
     end
   end
 
-  get 'map', to: 'maps#index' 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
