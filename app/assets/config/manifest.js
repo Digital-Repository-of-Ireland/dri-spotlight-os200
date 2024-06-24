5bcd9967871de7fa7754274f1f93048afba83503
Stuart Kenny <kennystu@gmail.com>
2024-06-24 17:36:36 +0100

    Add analytics

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,5 +2,6 @@
 //= link application.js
 //= link application.css
 //= link openseadragon-assets.js
+//= link cookieconsent-config.js
 //= link openlayers/theme/default/style.css
 //= link ol-layerswitcher/ol-layerswitcher.css
